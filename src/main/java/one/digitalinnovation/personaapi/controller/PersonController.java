033cd2b95ae91703e7971e178910e7806a63db20
Eduardo <eduardo-lmendes@hotmail.com>
2021-07-31 00:41:50 -0300

    v3

--- src/main/java/one/digitalinnovation/personaapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personaapi/controller/PersonController.java
@@ -1,32 +1,29 @@
 package one.digitalinnovation.personaapi.controller;
 
-import one.digitalinnovation.personaapi.dto.MessageResponseDTO;
-import one.digitalinnovation.personaapi.entity.Personi;
-import one.digitalinnovation.personaapi.repository.PersonRepository;
+import one.digitalinnovation.personaapi.dto.request.PersonDTO;
+import one.digitalinnovation.personaapi.dto.response.MessageResponseDTO;
+import one.digitalinnovation.personaapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
-    private PersonRepository personRepository;
+    private PersonService personService;
 
     @Autowired
-    public PersonController(PersonRepository personRepository) {
-
-        this.personRepository = personRepository;
+    public PersonController(PersonService personService) {
+        this.personService = personService;
     }
 
+
     @PostMapping
-    public MessageResponseDTO createPerson(@RequestBody Personi personi){
-        Personi savePersoni = personRepository.save(personi);
-        return MessageResponseDTO
-                .builder()
-                .message("Create person with ID " + savePersoni.getId())
-                .build();
+    @ResponseStatus(HttpStatus.CREATED)
+    public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO){
+        return personService.createPerson(personDTO);
     }
 }
